cbf7acbe9bd3868002fe5088523a3cc45c7adaa4
youngjin-hong <hyjinn1003@gmail.com>
2025-09-28 19:23:20 +0900

    Feat: PDF 사이즈 A4 로 수정

--- scripts/export-pdf.js
+++ scripts/export-pdf.js
@@ -3,13 +3,12 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1600, height: 3000 });
+  // await page.setViewport({ width: 1600, height: 3000 });
   await page.goto('http://localhost:3000', { waitUntil: 'domcontentloaded', timeout: 60000 });
   await new Promise((resolve) => setTimeout(resolve, 6000));
   await page.pdf({
     path: '/Users/youngjin-hong/Desktop/resume/resume-draft.pdf',
-    width: '1600',
-    height: '1587',
+    format: 'A4',
     printBackground: true, // 배경색, 배경이미지까지 출력
   });
   await browser.close();
